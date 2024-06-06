030810f7877a416020e86f30da6c9ab63a50adcf
papkok <papkokking@gmail.com>
2024-06-06 19:34:05 +0330

    the second comitment the code is working at this time

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def get_db():
 
 
 
-@app.post("/cous/", response_model=schemas.Coursbase) 
+@app.post("/regcous/", response_model=schemas.Coursbase) 
 def create_cours(cours: schemas.Coursbase, db: Session = Depends(get_db)) -> schemas.Coursbase:
     resp = []
     if int(len(str(cours.CID))) > 5:
@@ -35,12 +35,14 @@ def create_cours(cours: schemas.Coursbase, db: Session = Depends(get_db)) -> sch
     if db_course:
         raise HTTPException(status_code=400, detail="CID already registerd")
     return crud.create_course(db=db, course=cours)
-@app.get("/delstu/{CID}",response_model=schemas.Coursbase)
+
+
+@app.get("/delcous/{CID}",response_model=schemas.Coursbase)
 def delete_cours(CID:int,cours:schemas.Coursbase ,db:Session=Depends(get_db)):
     db_course = crud.get_course(db,cid=CID)
     if  db_course is None:
         raise HTTPException(status_code=400,detail="CID Dose not exist")
-    return crud.delete_cous(db=db, cid=cours)
+    return crud.delete_cous(db=db, cid=CID)
 
 
 
